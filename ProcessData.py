7c03064c920a15172dc553f11bebb7d03f11cd95
AlexHernandez2006 <ahernandezlopez@unomaha.edu>
2025-04-06 17:47:48 +0000

    Need to work on major year

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name:Alex Hernandez Lopez
+#Date:4/6/25
+#Assignment:Lab 8
 
 import random
 
@@ -12,12 +12,29 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  for line in inFile:
+    data = line.split()
+    first = data[0]
+    last = data[1]
+    idNum = data[3]
 
-
+    student_id = makeID(first, last, idNum)
+    output = last + "," + first + "," + student_id + "\n"
+    outFile.write(output)
 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
+
+def makeID(first, last, idNum):
+  while len(last) < 5:
+    last = last + "X"
+
+  idLen = len(idNum)
+  id = first[0] + last + idNum[idLen - 3: ]
+
+  return id
+
 
 if __name__ == '__main__':
   main()
